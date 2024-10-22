73edbaa843377c6508079d476dde7bb294238de2
Aleksandr Smelik <suntrope55@gmail.com>
2024-10-22 13:59:37 +0600

    feat: add Postgres Test Database, and env for it

--- src/core/config.py
+++ src/core/config.py
@@ -8,17 +8,30 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file=".env", env_ignore_empty=True, extra="ignore")
 
-    POSTGRES_HOST: str
+    POSTGRES_HOST: str = "localhost"
     POSTGRES_PORT: int
     POSTGRES_DB: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
+
+    POSTGRES_TEST_HOST: str = "localhost"
+    POSTGRES_TEST_DB: str
+    POSTGRES_TEST_USER: str
+    POSTGRES_TEST_PASSWORD: str
+    POSTGRES_TEST_PORT: int
 
     @property
     def SQLALCHEMY_DATABASE_URI(self) -> str:
         return (
             f"postgresql+asyncpg://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
             f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
+
+    @property
+    def SQLALCHEMY_TEST_DATABASE_URI(self) -> str:
+        return (
+            f"postgresql+asyncpg://{self.POSTGRES_TEST_USER}:{self.POSTGRES_TEST_PASSWORD}"
+            f"@{self.POSTGRES_TEST_HOST}:{self.POSTGRES_TEST_PORT}/{self.POSTGRES_TEST_DB}"
         )
 
 
