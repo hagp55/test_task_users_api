d5b1f513ccf8fd5488dbe5d5dab755255e35ca16
Aleksandr Smelik <suntrope55@gmail.com>
2024-10-23 13:49:45 +0600

    feat: add test for paginate

--- tests/users/test_get_users.py
+++ tests/users/test_get_users.py
@@ -1,3 +1,4 @@
+import pytest
 from httpx import AsyncClient
 
 from src.users.models import User
@@ -18,6 +19,52 @@ async def test_users_list(async_client: AsyncClient, create_list_users: tuple[Us
 
     assert response.status_code == 200
     assert len(json_response_data) == 25
+
+
+@pytest.mark.parametrize(
+    "page, size, expected_count_users",
+    [
+        (1, 10, 10),
+        (1, 3, 3),
+        (2, 8, 8),
+        (8, 2, 2),
+        (100, 25, 0),
+        (52, 100, 0),
+    ],
+)
+async def test_users_list_with_paginate(
+    async_client: AsyncClient,
+    create_list_users: tuple[User, ...],
+    page: int,
+    size: int,
+    expected_count_users: int,
+) -> None:
+    response = await async_client.get(f"/users/?page={page}&size={size}")
+    json_response_data = response.json()
+    users_list = [
+        UserFromDB.model_validate(user)
+        for user in create_list_users[(page - 1) * size : size * page]
+    ]
+    user_list_response = [UserFromDB.model_validate(user) for user in json_response_data]
+
+    assert response.status_code == 200
+    assert user_list_response == users_list
+    assert len(json_response_data) == expected_count_users
+
+
+@pytest.mark.parametrize(
+    "page, size",
+    [
+        (0, 2),
+        (-2, 10),
+    ],
+)
+async def test_not_successfully_users_list_with_paginate(
+    async_client: AsyncClient, create_list_users: tuple[User, ...], page: int, size: int
+) -> None:
+    response = await async_client.get(f"/users/?page={page}&size={size}")
+
+    assert response.status_code == 422
 
 
 async def test_user_detail(async_client: AsyncClient, create_list_users: tuple[User, ...]) -> None:
